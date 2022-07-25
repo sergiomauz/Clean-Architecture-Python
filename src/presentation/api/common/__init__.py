c1ce6c6d332b5bf11fe6784d0fb9c730c2a0879c
Sergio Mauricio Zambrano <sergio.mauz88@gmail.com>
2022-07-25 01:35:01 -0500

    Avance

--- src/presentation/api/common/__init__.py
+++ src/presentation/api/common/__init__.py
@@ -3,7 +3,6 @@
 """
 
 from .api_message_vm import ApiMessageVm
-from .api_error_vm import ApiErrorVm
 from .api_response_vm import ApiResponseVm
 from .api_result_vm import ApiResultVm
 from .constants import Constants
